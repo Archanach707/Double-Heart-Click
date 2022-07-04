fc235a0ec7c914e9494e96fa7697b4105c66f85c
Archanach707 <54771334+Archanach707@users.noreply.github.com>
2022-07-04 22:02:02 +0530

    complete project

--- js/index.js
+++ js/index.js
@@ -2,6 +2,7 @@ const loveMe = document.querySelector('.loveMe')
 const times = document.querySelector('#times')
 
 let clickTime = 0
+let timesClicked = 0
 
 loveMe.addEventListener('click', (e) => {
   if(clickTime === 0) {
@@ -26,13 +27,19 @@ const createHeart = (e) => {
   const x = e.clientX
   const y = e.clientY
 
-  const leftOffset = e.target.leftOffset
-  const topOffset = e.target.topOffset
+  const leftOffset = e.target.offsetLeft
+  const topOffset = e.target.offsetTop
 
   const xInside = x - leftOffset
   const yInside = y - topOffset
 
-  console.log(xInside, yInside)
+  heart.style.top = `${yInside}px`
+  heart.style.left = `${yInside}px`
 
+  loveMe.appendChild(heart)
+
+  times.innerHTML = ++timesClicked
+
+  setTimeout(() => heart.remove(), 1000)
 }
 
